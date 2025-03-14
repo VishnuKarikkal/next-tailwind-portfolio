acc82b7e004d6ad5543c4338116e245d6bbfb618
Vishnu S <vishnu.ks@globallogic.com>
2025-03-14 11:30:45 +0530

    all files and changes

---
+++ pages/services/index.js
@@ -0,0 +1,31 @@
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+const Services = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8">
+              My Services <span className="text-accent">.</span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+              lorem ihiahsdfnaksnf sdifhgaisf idfhiasf sadfhdui fasdjfha
+              fasjjhfa fakgfdamn dfukas fasdkufgasu.
+            </p>
+          </div>
+          <div className="w-full xl:max-w-[65%]">
+            <ServiceSlider />
+          </div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default Services;
---
+++ pages/work/index.js
@@ -0,0 +1,31 @@
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+const Work = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-12">
+              My work <span className="text-accent">.</span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+              lorem ihiahsdfnaksnf sdifhgaisf idfhiasf sadfhdui fasdjfha
+              fasjjhfa fakgfdamn dfukas fasdkufgasu.
+            </p>
+          </div>
+          <div className="w-full xl:max-w-[65%]">
+            <WorkSlider />
+          </div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default Work;
